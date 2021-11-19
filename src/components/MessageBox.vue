e361748d3a19b193a2a99de8ca6759949621efa4
zeyuan.cao <zcao@lyft.com>
2021-11-19 17:32:39 -0500

    scoped css within component to avoid global conflict

--- src/components/MessageBox.vue
+++ src/components/MessageBox.vue
@@ -3,3 +3,26 @@
     <div class="message-content">{{ content }}</div>
   </div>
 </template>
+
+<style scoped>
+  .message-wrapper {
+    position: fixed;
+    left: 0;
+    top: 0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(12, 12, 12, 0.33);
+  }
+
+  .message-content {
+    padding: 32px;
+    border-radius: 2px;
+    margin: 0 16px;
+    background-color: white;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.33);
+    display: block;
+  }
+</style>
